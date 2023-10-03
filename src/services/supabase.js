63f2a99602385cfb6bdc0dd30873f270c0ae131d
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-10-03 16:01:02 +0100

    supabase connection

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://yezerjfjewlflvmlcclm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllemVyamZqZXdsZmx2bWxjY2xtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMjg3NDksImV4cCI6MjAxMTkwNDc0OX0.DAJvaVubDg57NWFEadP593L65jVHwjJx1P-0eyVQ9m8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
